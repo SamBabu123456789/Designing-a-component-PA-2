c9d71c35f1afdba2e6353ee0865c90669a7712ba
SamBabu123456789 <sambabupolimetla@gmail.com>
2025-03-12 10:37:35 +0530

    done

---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+// write button card here
+import React from 'react'
+
+const button = () => {
+  return (
+    <div>
+      <button>View product</button>
+    </div>
+  )
+}
+
+export default button
